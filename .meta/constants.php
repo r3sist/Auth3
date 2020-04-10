3167352e06081213f0bd51940990500fc4e8dba4
r3sist <r3sist@users.noreply.github.com>
2020-04-10 20:17:04 +0200

    Update .meta

--- .meta/constants.php
+++ .meta/constants.php
@@ -1,15 +1,8 @@
 <?php
-// Table name of app specific/custom user data. (string)
 define('AUTH3_USERDATATABLE', 'users');
-
-// Throttling. (bool)
 define('AUTH3_THROTTLING', false);
-
-// Invite code for registration. If empty string, captcha used. (string)
 define('AUTH3_INVITECODE', '');
-
-// Email verification email send from. (string)
+define('AUTH3_CAPTCHA', false);
 define('AUTH3_EMAILFROM', '');
-
-// Store email in DB and send verification email during registration
 define('AUTH3_EMAIL_REQUIRED', true);
+
