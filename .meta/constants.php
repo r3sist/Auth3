e670ff0f74284cc2302f0f66c264a00eed2d209a
r3sist <r3sist@users.noreply.github.com>
2020-04-10 13:50:50 +0200

    Add meta file for constants

---
+++ .meta/constants.php
@@ -0,0 +1,12 @@
+<?php
+// Table name of app specific/custom user data. (string)
+define('AUTH3_USERDATATABLE', 'users');
+
+// Throttling. (bool)
+define('AUTH3_THROTTLING', false);
+
+// Invite code for registration. If empty string, captcha used. (string)
+define('AUTH3_INVITECODE', '');
+
+// Email verification email send from. (string)
+define('AUTH3_EMAILFROM', '');
